87013ea341ebd34efebbd0594585164fbe23615a
Matthias Lersch <matthias.lersch@solvians.com>
2017-09-13 15:56:46 +0200

    debug

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
+ARRAY=()
+for INSTANCE in $KEYCLOAK_NODE_IP
+do
+        IP=$(curl -sb -H "Accept: application/json" "http://rancher-metadata/latest/containers/$INSTANCE/primary_ip")
+        ARRAY+=($IP)
+done
+export KEYCLOAK_NODE_IP=$(join_by ',' ${ARRAY[@]})
+
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+exec /opt/jboss/keycloak/bin/standalone.sh $@
+exit $?
